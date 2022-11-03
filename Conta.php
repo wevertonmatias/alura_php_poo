aeb787556af1503f8bb3cc72ffb27711f0e31c7d
Weverton Matias <weverton-matias@hotmail.com>
2022-11-03 16:36:13 -0300

    Avançando com classes, definindo atributos.

--- Conta.php
+++ Conta.php
@@ -2,5 +2,21 @@
 
 class Conta
 {
-
+    public string $cpfTitular;
+    public string $nomeTitular;
+    public float $saldo;
 }
+
+$primeiraConta = new Conta();
+$primeiraConta->cpfTitular = '123.456.789-10';
+$primeiraConta->nomeTitular = 'Weverton Matias';
+$primeiraConta->saldo = 1500;
+
+var_dump($primeiraConta);
+
+$segundaConta = new Conta();
+$segundaConta->cpfTitular = '147.258.369.12';
+$segundaConta->nomeTitular = 'Catrina Cebi';
+$segundaConta->saldo = 7500;
+
+var_dump($segundaConta);
