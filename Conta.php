2f9762687c4eeabc960b5fe2d846781eada871d9
Weverton Matias <weverton-matias@hotmail.com>
2022-11-04 13:02:18 -0300

    Avançanco com classes, criando o primeiro método.

--- Conta.php
+++ Conta.php
@@ -5,18 +5,13 @@ class Conta
     public string $cpfTitular;
     public string $nomeTitular;
     public float $saldo;
+
+    public function sacar($valor){
+        if($this->saldo <= $valor){
+            echo "Saldo indisponível";
+        }else{
+            $this->saldo -= $valor;
+        }
+    }
+
 }
-
-$primeiraConta = new Conta();
-$primeiraConta->cpfTitular = '123.456.789-10';
-$primeiraConta->nomeTitular = 'Weverton Matias';
-$primeiraConta->saldo = 1500;
-
-var_dump($primeiraConta);
-
-$segundaConta = new Conta();
-$segundaConta->cpfTitular = '147.258.369.12';
-$segundaConta->nomeTitular = 'Catrina Cebi';
-$segundaConta->saldo = 7500;
-
-var_dump($segundaConta);
