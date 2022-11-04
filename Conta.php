becdf5e51584bdcb3fc1f0512f4d1e03248f6078
Weverton Matias <weverton-matias@hotmail.com>
2022-11-04 13:15:42 -0300

    Classes, adicionando a função depositar e regra de saldo zero.

--- Conta.php
+++ Conta.php
@@ -4,13 +4,21 @@ class Conta
 {
     public string $cpfTitular;
     public string $nomeTitular;
-    public float $saldo;
+    public float $saldo = 0;
 
     public function sacar($valor){
         if($this->saldo <= $valor){
             echo "Saldo indisponível";
         }else{
             $this->saldo -= $valor;
+        }
+    }
+
+    public function depositar($valor){
+        if($valor <= 0){
+            echo "Erro: valor negativo.";
+        }else{
+            $this->saldo += $valor;
         }
     }
 
