34f94bae2e5afa9bb2569750c7da48e6495b80f0
Weverton Matias <weverton-matias@hotmail.com>
2022-11-11 17:06:26 -0300

    Classes, separando classe CPF para um reaproveitamento futuro.

--- conta_oper.php
+++ conta_oper.php
@@ -2,11 +2,17 @@
 
 require_once "Conta.php";
 require_once "Titular.php";
+require_once "Cpf.php";
+
+$cpfTitular = new Cpf('111.111.111-01');
+$titular = new Titular('Weverton1', $cpfTitular);
+$minhaConta = new Conta($titular);
+
+$cpfTitular2 = new Cpf('111.111.111-9');
+$titular2 = new Titular('Weverton1', $cpfTitular2);
+$minhaConta2 = new Conta($titular2);
 
 
-$minhaConta = new Conta(new Titular('Weverton1', '111', ));
-$minhaConta2 = new Conta(new Titular('Weverton2', '222'));
-$minhaConta3= new Conta(new Titular('Weverton3', '333'));
+//$minhaConta2 = new Conta(new Titular('Weverton2', '222'));
+//$minhaConta3= new Conta(new Titular('Weverton3', '333'));
 
-echo $minhaConta2->mostrarNomeTitular() . PHP_EOL;
-echo $minhaConta2->mostrarCpfTitular() . PHP_EOL;
